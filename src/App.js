9b1dc14807747c865b3818c14a77e1a7876ca73f
ozanorkun <ozanorkun1@gmail.com>
2021-01-30 01:04:29 +0300

    styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
 	return (
 		<div className="App">
-			<h3 style={{ color: '#ffffff' }}>Periyodik Tablo</h3>
+			<h3 style={{ color: '#ffffff' }}>Periyodik Tablo v1.0</h3>
 			<PeriodicTable />
 		</div>
 	);
